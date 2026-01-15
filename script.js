ad3c1f0e943ddde93f8788f8bb52dc837d91e201
william <williamnjati420@gmail.com>
2026-01-15 09:44:24 +0300

    setup - fetching from the server"

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-fetch ("https://api.dictionaryapi.dev/api/v2/entries/en/<word>")
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
+// Setup
+
+let url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+let updatedUrl = "";
+
+function search () {
+    let searchWord = document.getElementById("search-word").value;
+    let updatedUrl = url.concat(searchWord);
+    console.log (updatedUrl);
+    
+    getData();
+}
+
+
+//Hitting the endpoint Using async await
+
+function getData () {       
+    try {
+        let response = fetch (updatedUrl)
+        console.log (response);
+
+        if (!response.ok) {
+            throw new Error (`Response status: ${response.status}`);
+        }
+        let result = response.json()
+        console.log(result)
+    } catch (error) {
+            console.log(error.message);
+        }
+};
+
+
